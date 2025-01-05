404e80f028123bc873d0593ca1bc0a9fa4dc3a9c
github YSWEngineer <yoshiwo.may15@gmail.com>
2025-01-05 18:32:57 +0900

    Refactor code to improve readability

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,34 +1,32 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-scores = ARGV[0].split(',')
-shots = scores.map { |score| score == 'X' ? 10 : score.to_i }
+shots = ARGV[0].split(',').map { |shot| shot == 'X' ? 10 : shot.to_i }
 
-frames = []
 current_frame = []
+frames = [current_frame]
 
 shots.each do |shot|
   current_frame << shot
   if current_frame[0] == 10 || current_frame.size == 2
-    frames << current_frame
     current_frame = []
+    frames << current_frame
   end
 end
 
 frames << current_frame unless current_frame.empty?
 
-total_score = 0
+total_score = frames.take(10).each_with_index.sum do |frame, i|
+  next_frame = frames[i + 1] || []
+  after_next_frame = frames[i + 2] || []
 
-frames.take(10).each_with_index do |frame, i|
-  next_frame = frames[i + 1]
-  frame_after_next = frames[i + 2]
-
-  if frame[0] == 10
-    total_score += next_frame.size == 1 ? 10 + next_frame.sum + frame_after_next[0] : 10 + next_frame.take(2).sum
+  if frame.first == 10
+    following_shots = next_frame + after_next_frame
+    10 + following_shots.take(2).sum
   elsif frame.sum == 10
-    total_score += 10 + next_frame.first
+    10 + next_frame.first
   else
-    total_score += frame.sum
+    frame.sum
   end
 end
 
