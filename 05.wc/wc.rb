d5b5d2e573b93b67bf5208401a17e3be232a0a06
github YSWEngineer <yoshiwo.may15@gmail.com>
2026-04-22 17:28:00 +0900

    レビューに基づきリファクタリング

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -6,70 +6,61 @@ require 'optparse'
 
 def main
   options = {}
-
   opt = OptionParser.new
-  opt.on('-l') { options[:l] = true }
-  opt.on('-w') { options[:w] = true }
-  opt.on('-c') { options[:c] = true }
+  opt.on('-l') { options[:lines] = true }
+  opt.on('-w') { options[:words] = true }
+  opt.on('-c') { options[:bytes] = true }
   opt.parse!(ARGV)
-
   filenames = ARGV
-
   if filenames.empty?
-    process_stdin(options)
+    count_from_stdin(options)
   else
-    process_files(filenames, options)
+    display_file_counts(filenames, options)
   end
 end
 
-def print_count(value)
-  print value.to_s.rjust(8)
-end
-
-def print_counts(counts, options, filename = nil)
-  [
-    [counts[:lines], 'l'],
-    [counts[:words], 'w'],
-    [counts[:bytes], 'c']
-  ].each do |value, option_char|
-    print_count(value) if options.empty? || options[option_char.to_sym]
-  end
-  print " #{filename}" if filename
-  print "\n"
-end
-
-def count_content(content)
-  {
-    lines: content.lines.count,
-    words: content.split.size,
-    bytes: content.bytesize
-  }
-end
-
-def process_stdin(options)
+def count_from_stdin(options)
   content = $stdin.read
   counts = count_content(content)
   print_counts(counts, options)
 end
 
-def process_file(filename)
-  content = File.read(filename)
-  count_content(content)
-end
-
-def process_files(filenames, options)
-  total = { lines: 0, words: 0, bytes: 0 }
-
+def display_file_counts(filenames, options)
+  total = { lines: 0, words: 0, bytes: 0, name: 'total' }
   filenames.each do |filename|
-    counts = process_file(filename)
+    counts = count_from_file(filename)
     total[:lines] += counts[:lines]
     total[:words] += counts[:words]
     total[:bytes] += counts[:bytes]
-
-    print_counts(counts, options, filename)
+    print_counts(counts, options)
   end
+  print_counts(total, options) if filenames.size > 1
+end
 
-  print_counts(total, options, 'total') if filenames.size > 1
+def count_from_file(filename)
+  content = File.read(filename)
+  count_content(content, filename)
+end
+
+def count_content(content, name = nil)
+  {
+    lines: content.lines.count,
+    words: content.split.size,
+    bytes: content.bytesize,
+    name: name
+  }
+end
+
+def print_counts(counts, options)
+  %i[lines words bytes].each do |key|
+    print format_count(counts[key]) if options.empty? || options[key]
+  end
+  print " #{counts[:name]}" if counts[:name]
+  print "\n"
+end
+
+def format_count(value)
+  value.to_s.rjust(8)
 end
 
 main
